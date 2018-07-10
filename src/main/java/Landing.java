77520d25242e5a0f6f3ef5d3617d4d1fb2f44dcf
jenisonleo <jenisongracious@gmail.com>
2018-07-10 15:47:34 +0530

    jdbc added

--- src/main/java/Landing.java
+++ src/main/java/Landing.java
@@ -1,8 +1,10 @@
+import com.mysql.jdbc.Driver;
 import com.opensymphony.xwork2.Action;
 
 public class Landing implements Action {
 	@Override
 	public String execute() throws Exception {
+		Driver driver = new Driver();
 		return "success";
 	}
 }
